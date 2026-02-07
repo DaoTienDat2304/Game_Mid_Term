96e64d9825d84befc6bf7bb30aa32b7d113a2f46
DaoTienDat2304 <tiendatt230405@gmail.com>
2026-02-07 17:06:14 +0700

    DemoPart4

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -1,16 +1,26 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-public float flySpeed;
+    public float flySpeed = 8f;
+    public int damage = 1;
 
-// Update is called once per frame
-void Update()
-{
-var newPosition = transform.position;
-newPosition.y += Time.deltaTime * flySpeed;
-transform.position = newPosition;
-}
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.y += flySpeed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Health health))
+        {
+            health.TakeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
 }
